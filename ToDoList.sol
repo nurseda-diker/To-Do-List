77cb8300a132c29aa5cdf1c1edf56d1da9d08dbb
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2022-09-30 10:50:46 +0300

    Create ToDoList.sol

---
+++ ToDoList.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.3;
+//Insert,update,read from array structs
+
+contract ToDoList{
+    struct ToDo{
+        string text;
+        bool completed;
+    }
+
+    ToDo[] public todos;
+    //todos array oluşturma
+    function create(string calldata _text) external {
+        todos.push(ToDo({
+            text:_text,
+            completed:false
+        }));
+    }
+
+    //yapılacakları güncelleme
+    function updateText(uint _index,string calldata _text) external {
+        todos[_index].text=_text;
+    }
+
+
+    function get(uint _index) external view returns (string memory,bool){
+        // storage - 29397 gas --- cheaper option
+        // memory - 29480 gas
+        ToDo memory todo = todos[_index];
+        return (todo.text,todo.completed);
+    }
+
+    //tamamlanma durumunu güncelleme
+    function toggleCompleted(uint _index) external {
+        todos[_index].completed=!todos[_index].completed;
+    }
+
+}
+
